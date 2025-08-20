7143d77e293dfca6ad22c9c226d665cac9fd2f12
Shlomo <sh.z.wind@gmail.com>
2025-08-20 09:12:57 +0300

    created dal, conection, object

---
+++ services/data_loader/conection.py
@@ -0,0 +1,22 @@
+import os
+from pymongo import MongoClient
+
+
+class Connection:
+    def __init__(self):
+        self.db_name = os.getenv("MONGO_DB_NAME", "my_database")
+        self.collection_name = os.getenv("MONGO_COLLECTION_NAME", "my_collection")
+        self.host = os.getenv("MONGO_URL", "mongodb://localhost:27017/")
+        self.port = os.getenv("MONGO_PORT", "27017")
+        self.username = os.getenv("MONGO_USERNAME", "root")
+        self.password = os.getenv("MONGO_PASSWORD", "password")
+
+        self.client = MongoClient(
+            host=self.host,
+            port=int(self.port),
+            username=self.username,
+            password=self.password
+        )
+        self.db = self.client[self.db_name]
+        self.collection = self.db[self.collection_name]
+
---
+++ services/data_loader/dal.py
@@ -0,0 +1,18 @@
+from services.data_loader.conection import Connection
+
+class Dal:
+    def __init__(self):
+        self.connection = Connection()
+        self.query = None
+
+    def create(self):
+        pass
+
+    def read(self):
+        pass
+
+    def update(self):
+        pass
+
+    def delete(self):
+        pass
